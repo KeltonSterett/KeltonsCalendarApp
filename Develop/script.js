7ca6d39113621fb65fc364ecd1cc07132e1e6da8
KeltonSterett <keltonsterett24@gmail.com>
2023-03-25 20:25:28 -0600

    added a loop to insert all the html for the times in java, added ternarys to display correct tense of the classes

--- Develop/script.js
+++ Develop/script.js
@@ -8,13 +8,45 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  var hours = ["9", "10", "11"]
+
+
+
+
+ 
+// inputing html using for loop to display hours of the day and their text boxes
+// added multiple ternary's (if/else) statments to make the classes display depending on the time of day
+    for (let index = 0; index < 9; index++) {
+      var timeOfDay = 8 + index
+      var now = dayjs()
+      // now.$H = 11 (this was to test)
+      var tense = now.$H-1 > timeOfDay ? 'past' : 'future'
+      tense = now.$H-1 === timeOfDay ?  'present' : tense
+      // % = remainder not division, divides and returns the remainder 
+      timeOfDay = timeOfDay % 12
+      timeOfDay++
+      var afterNoon = timeOfDay > 8 ? 'am' : 'pm'
+      afterNoon = timeOfDay === 12 ? 'pm' : afterNoon
+
+     
+
+// this makes it dynamic and not set to only one (time of the day)sets differnt time on differnt rows
+      var timeblock = `<div id="hour-${timeOfDay}" class="row time-block ${tense}">
+      <div class="col-2 col-md-1 hour text-center py-3">${timeOfDay}${afterNoon}</div>
+      <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+      <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+        <i class="fas fa-save" aria-hidden="true"></i>
+      </button>
+    </div>`
+    jQuery("#time-blocks").append(timeblock)
+    }
+
+  var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
   for (let index = 0; index < hours.length; index++) {
     var hour = hours[index];
     var hourI = localStorage.getItem("hour-" + hour)
   jQuery("#hour-" + hour + " .description").val(hourI);
   }
-
+// saves text in boxes to local storage
   jQuery('.saveBtn').on("click", function (event) {
     var currentTarget = jQuery(event.currentTarget)
     var parent = currentTarget.parent(".time-block")
