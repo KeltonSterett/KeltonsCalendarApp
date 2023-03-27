964d8c989d5f8ee56387e548e390b186fe1e36f3
KeltonSterett <keltonsterett24@gmail.com>
2023-03-26 19:50:57 -0600

    added a jQuery in JS to display current date at the top of the page using an ID, cleaned up code and comments

--- Develop/script.js
+++ Develop/script.js
@@ -2,59 +2,46 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // inputing html using for loop to display hours of the day and their text boxes
+  // added multiple ternary's (if/else) statments to make the classes display depending on the time of day
+  for (let index = 0; index < 9; index++) {
+    var timeOfDay = 8 + index;
+    var now = dayjs();
+    // now.$H = 11 (this was to test if classes were working correctly)
+    var tense = now.$H - 1 > timeOfDay ? 'past' : 'future';
+    tense = now.$H - 1 === timeOfDay ? 'present' : tense;
+    // % = remainder not division, divides and returns the remainder
+    timeOfDay = timeOfDay % 12;
+    timeOfDay++;
+    var afterNoon = timeOfDay > 8 ? 'am' : 'pm';
+    afterNoon = timeOfDay === 12 ? 'pm' : afterNoon;
 
-
-
-
- 
-// inputing html using for loop to display hours of the day and their text boxes
-// added multiple ternary's (if/else) statments to make the classes display depending on the time of day
-    for (let index = 0; index < 9; index++) {
-      var timeOfDay = 8 + index
-      var now = dayjs()
-      // now.$H = 11 (this was to test)
-      var tense = now.$H-1 > timeOfDay ? 'past' : 'future'
-      tense = now.$H-1 === timeOfDay ?  'present' : tense
-      // % = remainder not division, divides and returns the remainder 
-      timeOfDay = timeOfDay % 12
-      timeOfDay++
-      var afterNoon = timeOfDay > 8 ? 'am' : 'pm'
-      afterNoon = timeOfDay === 12 ? 'pm' : afterNoon
-
-     
-
-// this makes it dynamic and not set to only one (time of the day)sets differnt time on differnt rows
-      var timeblock = `<div id="hour-${timeOfDay}" class="row time-block ${tense}">
+    // this makes it dynamic and not set to only one (time of the day)sets differnt time on differnt rows
+    var timeblock = `<div id="hour-${timeOfDay}" class="row time-block ${tense}">
       <div class="col-2 col-md-1 hour text-center py-3">${timeOfDay}${afterNoon}</div>
       <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
       <button class="btn saveBtn col-2 col-md-1" aria-label="save">
         <i class="fas fa-save" aria-hidden="true"></i>
       </button>
-    </div>`
-    jQuery("#time-blocks").append(timeblock)
-    }
+    </div>`;
+    jQuery('#time-blocks').append(timeblock);
+  }
 
-  var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+  var hours = ['9', '10', '11', '12', '1', '2', '3', '4', '5'];
   for (let index = 0; index < hours.length; index++) {
     var hour = hours[index];
-    var hourI = localStorage.getItem("hour-" + hour)
-  jQuery("#hour-" + hour + " .description").val(hourI);
+    var hourI = localStorage.getItem('hour-' + hour);
+    jQuery('#hour-' + hour + ' .description').val(hourI);
   }
-// saves text in boxes to local storage
-  jQuery('.saveBtn').on("click", function (event) {
-    var currentTarget = jQuery(event.currentTarget)
-    var parent = currentTarget.parent(".time-block")
-    var parentId = parent.attr("id")
-    var description = parent.find(".description")
-    var value = description.val()
-localStorage.setItem(parentId, value)
-return false;
+  // saves text in boxes to local storage
+  jQuery('.saveBtn').on('click', function (event) {
+    var currentTarget = jQuery(event.currentTarget);
+    var parent = currentTarget.parent('.time-block');
+    var parentId = parent.attr('id');
+    var description = parent.find('.description');
+    var value = description.val();
+    localStorage.setItem(parentId, value);
+    return false;
   });
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -67,5 +54,7 @@ return false;
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page. 
+  // TODO: Add code to display the current date in the header of the page.
+  //  used dayjs to display the current date
+  jQuery('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'));
 });
